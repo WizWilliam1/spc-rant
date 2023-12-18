8bcb80012cb0cc43e174ef02f9eb8d52b9be4b6b
WizWilliam1 <144577981+WizWilliam1@users.noreply.github.com>
2023-12-18 12:19:55 +0800

    Add files via upload
    Added Comments BISAYA EXPLANATION

--- Main/database.php
+++ Main/database.php
@@ -1,22 +1,23 @@
 <?php
-class Database {
-    private $conn;
-    private $servername = "localhost";
-    private $username = "root";
+class Database { //Bali mao ni ang PARENT CLASS which is si "Database", then ang curly braces kay dre ipang intak ang tanan properties ug methods.
+    private $conn; //Dre nag start ang iyang PROPERTIES
+    private $servername = "localhost"; //Different sa PUBLIC ug PRIVATE [Public = Ma used sha ug iyang methods bisag asa nga maka access sa object]
+    private $username = "root"; //Then ang PRIVATE [Kebali ma access ra sha within the class kung asa sha gi defined(So Other purpose nganong ni gamit ani especially kay naa ta sa sensitive data which is ang database then by using the private kay ma protect ni niya and mas secure)]
     private $password = "";
     private $dbname = "database";
 
-    public function __construct() {
-        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+//Then mao na dayon ni ang METHODS
+    public function __construct() { //So ang gamit niya is, iyang eh automatically call ni niya nga object kebali if mag create kag method para sa object dadto sa class niya
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname); //So bali ang "$this" kay gamiton sha para ma access ang current object of the class [so mura ra shag tool bitaw nga aron ma gamit niya ang "$username"]
 
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
 
-        session_start();
+        session_start(); //Bali mura ni shag sa engine like atong yabihan bitaw nga, pag once tawagon ni sha kay mo start na dayon ang iyang session like, kunwari nag login ka then na save imong data nga naka login naka tas pag ma tawag ni then auto start na iyang session.
 
-        if (isset($_SESSION['Username'])) {
-            $this->username = $_SESSION['Username'];
+        if (isset($_SESSION['Username'])) { //So bali ang gamit sa "isset" kay eh check niya if ang variable kay "empty" or naa nay naka "set/declared"
+            $this->username = $_SESSION['Username']; //Tawagon nya si --> Username [Start ang Session (Check Username)]
         }
     }
 
